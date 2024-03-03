310b4e049899f00f481df059c8f38516d58fd72e
eljoserass <jose-antonio.rodriguez-assalone@epitech.eu>
2024-03-03 00:56:57 +0000

    [MOD] model_caller

--- src/model_caller.py
+++ src/model_caller.py
@@ -20,57 +20,6 @@ def list_to_dict(models_list: list):
 class ModelCalller:
     def __init__(self):
         self.poe_api_key = settings.POE_API_TOKEN
-        # claude's models are down
-        models_list =  [
-            {
-                "id": 0,
-                "name": "GPT-3.5-Turbo",
-                "provider": "poe"
-            },
-            {
-                "id": 1,
-                "name": "GPT-4",
-                "provider": "poe"
-            },
-            {
-                "id": 2,
-                "name": "Mistral-Large",
-                "provider": "poe"
-            },
-            {
-                "id": 3,
-                "name": "Mixtral-8x7b-Groq",
-                "provider": "poe"
-            },
-            {
-                "id": 4,
-                "name": "Gemini-Pro",
-                "provider": "poe"
-            },
-            {
-                "id": 5,
-                "name": "Llama-2-70b",
-                "provider": "poe"
-            },
-            {
-                "id": 6,
-                "name": "mistralai/mistral-7b-instruct-v0.2",
-                "provider": "replicate"
-            },
-            {
-                "id": 7,
-                "name": "mistralai/mixtral-8x7b-instruct-v0.1",
-                "provider": "replicate"
-            },
-            {
-                "id": 8,
-                "name": "Mixtral_CA",
-                "provider": "poe"
-            },
-            
-        ]
-        # models_list = 
-        self.models = list_to_dict(models_list)
     
     async def get_responses_poe(self, bot_name: str, messages: list[ProtocolMessage]) -> str:
         response = ""
@@ -89,11 +38,10 @@ class ModelCalller:
         return response
     
     def __call__(self, model_name: str, query: str, provider: str) -> str:
-        model = self.models.get(model_id)
-        if model["provider"] == "poe":
+        if provider == "poe":
             return asyncio.run(self.get_responses_poe(messages=[ProtocolMessage(role="user", content=query)],
-                                                      bot_name=model["name"])) # system prompt
-        elif model["provider"] == "replicate":
-            return self.get_responses_replicate(query=query, name=model["name"]) # maybe change this to list of messages and system prompt
-        elif model["provider"] == "ollama":
+                                                      bot_name=model_name)) # system prompt
+        elif provider == "replicate":
+            return self.get_responses_replicate(query=query, name=model_name) # maybe change this to list of messages and system prompt
+        elif provider == "ollama":
             return "not implemented"
