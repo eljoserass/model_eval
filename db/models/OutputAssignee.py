4f2240d560180ffa692bda19819c22dc3ac6b95c
Javier Sánchez <javier.sanchez-castro@epitech.eu>
2024-03-06 19:51:25 +0000

    [DELETE] file name swaped + add _

--- db/models/OutputAssignee.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from db.models.Base import Base
-
-
-class OutputAssignee(Base):
-    output_id = Column(Integer, ForeignKey("output.ID"))
-    assignee_id = Column(Integer, ForeignKey("assignee.ID"))
