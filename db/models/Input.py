6734a500e3d68d0d5e2f9ca3d8a943916cec1fc4
Javier Sánchez <javier.sanchez-castro@epitech.eu>
2024-03-06 19:50:05 +0000

    [UPDATE] Input Model

--- db/models/Input.py
+++ db/models/Input.py
@@ -5,6 +5,7 @@ from db.models.Base import Base
 from sqlalchemy import Enum as SQLAlchemyEnum
 from enum import Enum
 from sqlalchemy.dialects.mysql import TEXT
+from db.models.Session_Input import Session_Input
 
 
 class Label(Enum):
@@ -19,4 +20,6 @@ class Input(Base):
     """
     data = Column(String(500), nullable=False, unique=True)
     label = Column(SQLAlchemyEnum(Label), nullable=False)
+    
+    sessions = relationship("Session", secondary=Session_Input.__tablename__, back_populates="inputs")
     outputs = relationship("Output", back_populates="input")
