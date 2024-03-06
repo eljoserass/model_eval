609973b2f8d5cf0e97f6d56e7542b1bc511f09c3
Javier Sánchez <javier.sanchez-castro@epitech.eu>
2024-03-06 19:48:04 +0000

    [ADD] generator connector context

--- db/connector.py
+++ db/connector.py
@@ -1,10 +1,28 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from src.settings import settings
+from typing import Generator
+
 
 SQLALCHEMY_DATABASE_URL = settings.DB_URL.unicode_string()
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session = SessionLocal()
+#session = SessionLocal()
+
+
+from contextlib import contextmanager
+
+
+@contextmanager
+def get_session() -> Generator:
+    try:
+        db = SessionLocal()
+        yield db
+    except Exception as e:
+        print(f"Rolling Back [{e}]")
+        db.rollback()
+    finally:
+        print("Closing Session")
+        db.close()
