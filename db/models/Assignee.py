53081fbe0458bbc394c04c576d3b7d7ffdf1458d
Javier Sánchez <javier.sanchez-castro@epitech.eu>
2024-03-06 19:49:35 +0000

    [UPDATE] Assignee Model

--- db/models/Assignee.py
+++ db/models/Assignee.py
@@ -1,10 +1,11 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from db.models.Base import Base
-from db.models.OutputAssignee import OutputAssignee
+from db.models.Assignee_Output import Assignee_Output
+from db.models.Session_Assignee import Session_Assignee
 
 class Assignee(Base):
     name = Column(String(256), nullable=False)
     email = Column(String(256), nullable=False, unique=True)
-    outputs = relationship("Output", secondary=OutputAssignee.__tablename__, back_populates="assigness")
-    
+    outputs = relationship("Output", secondary=Assignee_Output.__tablename__, back_populates="assignees")
+    sessions = relationship("Session", secondary=Session_Assignee.__tablename__, back_populates="assignees")
